eb0fc103eadd8f3f9d24291945f1bea0b1ed7fef
Anton Starykov <starykov.anton@apriorit.com>
2019-05-31 18:59:11 +0300

    Green

--- tdd_intro/homework/02_ternary_numbers/test.cpp
+++ tdd_intro/homework/02_ternary_numbers/test.cpp
@@ -16,3 +16,12 @@ The last place in a ternary number is the 1's place. The second to last is the 3
 
 If your language provides a method in the standard library to perform the conversion, pretend it doesn't exist and implement it yourself.
 */
+
+
+int getTernaryNumberValue(std::string string) {
+    return 0;
+}
+
+TEST(TernaryNumbers, emptyStringReturnsZeroValue) {
+    ASSERT_EQ(getTernaryNumberValue(""), 0);
+}
