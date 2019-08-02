6f437271145839294dbff5000cf078fd5fa074c5
Anton Starykov <starykov.anton@apriorit.com>
2019-08-02 13:31:28 +0300

    Red

--- tdd_intro/homework/06_coffee/test.cpp
+++ tdd_intro/homework/06_coffee/test.cpp
@@ -123,3 +123,15 @@ TEST(Coffee, makeCapuchinoOfSizeBig) {
     EXPECT_CALL(source, AddMilk(140/3)).Times(1);
     machine.makeCapuchino(140);
 }
+
+TEST(Coffee, makeLatteOfSizeSmall) {
+    SourceOfIngredientsMock source;
+    CofeeMachine machine(&source);
+
+    EXPECT_CALL(source, SetCupSize(100)).Times(1);
+    EXPECT_CALL(source, AddWater(0, 90)).Times(1);
+    EXPECT_CALL(source, AddCoffee(100/2)).Times(1);
+    EXPECT_CALL(source, AddMilk(100/4)).Times(1);
+    EXPECT_CALL(source, AddMilkFoam(100/4)).Times(1);
+    machine.makeLatte(100);
+}
